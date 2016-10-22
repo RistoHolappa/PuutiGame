b5eea19ec1a73c0f412e04064ba2e264f63408c2
Risto Holappa <mail@ristoholappa.info>
2016-10-23 00:08:23 +0300

    Master class for objects

---
+++ src/entity/MapObject.java
@@ -0,0 +1,61 @@
+package entity;
+
+/**
+ * Created by GG Holappa on 23.10.2016.
+ */
+
+import tileMap.TileMap;
+
+
+public abstract class MapObject {
+
+    protected TileMap tilemap;
+    protected int tileSize;
+    protected double xmap;
+    protected double ymap;
+
+    protected double x;
+    protected double y;
+    protected double dx;
+    protected double dy;
+
+    protected int width;
+    protected int height;
+
+    protected int cwidth;
+    protected int cheight;
+
+    protected int currRow;
+    protected int currCol;
+    protected double xdest;
+    protected double ydest;
+    protected double xtemp;
+    protected double ytemp;
+    protected boolean topLeft;
+    protected boolean topRight;
+    protected boolean bottomLeft;
+    protected boolean bottomRight;
+
+    protected Animation animation;
+    protected int currentAction;
+    protected int previousAction;
+    protected boolean facingRight;
+
+    protected boolean left;
+    protected boolean right;
+    protected boolean up;
+    protected boolean down;
+    protected boolean jumping;
+    protected boolean jumping;
+    protected boolean falling;
+
+    protected double moveSpeed;
+    protected double maxSpeed;
+    protected double stopSteed;
+    protected double fallSpeed;
+    protected double maxFallSpeed;
+    protected double jumpStart;
+    protected double stopJumpSpeed;
+
+}
+
