a045d093a9ab4be922800750d7f5302182c7213a
Risto Holappa <mail@ristoholappa.info>
2016-10-23 12:46:00 +0300

    Continue making master class

--- src/entity/MapObject.java
+++ src/entity/MapObject.java
@@ -5,6 +5,8 @@ package entity;
  */
 
 import javafx.animation.Animation;
+import main.GamePanel;
+import tileMap.Tile;
 import tileMap.TileMap;
 
 import java.awt.*;
@@ -12,7 +14,7 @@ import java.awt.*;
 
 public abstract class MapObject {
 
-    protected TileMap tilemap;
+    protected TileMap tileMap;
     protected int tileSize;
     protected double xmap;
     protected double ymap;
@@ -74,5 +76,141 @@ public abstract class MapObject {
     public Rectangle getRectangle() {
         return new Rectangle((int) x - cwidth, (int) y - cheight, cwidth, cheight);
     }
-}
 
+    public void calculateCorners(double x, double y) {
+        int leftTile = (int) (x - cwidth / 2) / tileSize;
+        int rightTile = (int) (x + cwidth / 2 - 1) / tileSize;
+        int topTile = (int) (y - cheight / 2) / tileSize;
+        int bottomTile = (int) (y + cheight / 2 - 1) / tileSize;
+
+        int t1 = tileMap.getType(topTile, leftTile);
+        int tr = tileMap.getType(topTile, rightTile);
+        int b1 = tileMap.getType(bottomTile, leftTile);
+        int br = tileMap.getType(bottomTile, rightTile);
+
+        topLeft = t1 == Tile.BLOCKED;
+        topRight = tr == Tile.BLOCKED;
+        bottomLeft = b1 == Tile.BLOCKED;
+        bottomRight = br == Tile.BLOCKED;
+
+
+    }
+
+    public void checkTileMapCollision() {
+        currCol = (int) x / tileSize;
+        currRow = (int) y / tileSize;
+
+        xdest = x + dx;
+        ydest = y + dy;
+
+        xtemp = x;
+        ytemp = y;
+
+        calculateCorners(x, ydest);
+        if (dy < 0) {
+            if (topLeft || topRight) {
+                dy = 0;
+                ytemp = currRow * tileSize ? cheight / 2;
+            } else {
+                ytemp += dy;
+            }
+        }
+        if (dy > 0) {
+            if (bottomLeft || bottomRight) {
+                dy = o;
+                falling = false;
+                ytemp = (currRow + 1) * tileSize - cheight / 2;
+            } else {
+                ytemp += dy;
+            }
+        }
+
+        calculateCorners(xdest, y);
+
+        if (dx < 0) {
+            if (topLeft || bottomLeft) {
+                dx = 0;
+                xtemp = currCol * tileSize ? cwidth / 2;
+            } else {
+                xtemp += dx;
+            }
+        }
+        if (dx > 0) {
+            if (topRight || bottomRight) {
+                dx = 0;
+                xtemp = (currCol + 1) * tileSize + cwidth / 2;
+            }
+            if (!falling) {
+                calculateCorners(x, ydest + 1);
+                if (!bottomLeft && !bottomRight) {
+                    falling = true;
+                }
+            }
+        }
+
+    public int getx() {
+        return (int) x;
+    }
+
+    public int gety() {
+        return (int) y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getCWidth() {
+        return cwidth;
+    }
+
+    public int getCHeight() {
+        return cheight;
+    }
+
+    public void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setVector(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void setMapPosition() {
+        xmap = tileMap.getx();
+        ymap = tileMap.gety();
+    }
+
+    public void setLeft(boolean b) {
+        left = b;
+    }
+
+    public void setRight(boolean b) {
+        right = b;
+    }
+
+    public void setUp(boolean b) {
+        up = b;
+    }
+
+    public void setDown(boolean b) {
+        down = b;
+    }
+
+    public void setJumping(boolean b) {
+        jumping = b;
+    }
+
+    public boolean notOnScreen() {
+        return x + xmap + width < 0 || x + xmap - width > GamePanel.WIDTH
+                || y + ymap + height < 0
+                || y + ymap - height > GamePanel.HEIGHT;
+    }
+}
+}
